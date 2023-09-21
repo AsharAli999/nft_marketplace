ed18671622f82e5af5bbee1ecc99df90f5658954
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-09-21 22:44:30 +0500

    Fixed UI issues

--- src/frontend/components/MyPurchases.js
+++ src/frontend/components/MyPurchases.js
@@ -123,7 +123,6 @@ export default function MyPurchases({ marketplace, nft, account }) {
       </div>
 
 
-
       {isModalOpen && (
         <div className="fixed z-10 inset-0 overflow-y-auto">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
